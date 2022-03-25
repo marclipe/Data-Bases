82001052d8e5fbc891f687b8851fcc657d4eade0
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-03-24 21:07:51 -0300

    Cadastro

--- CursoEmVideo/pessoas.sql
+++ CursoEmVideo/pessoas.sql
@@ -1,15 +1,15 @@
-create database cadastro 
-default character set uf8 
+create database cadastro;
+default character set uf8
 default collate utf8_general_ci; 
 
 create table pessoas(
-id int not null auto_increment, 
-nome varchar(30) not null, 
-nascimento date,
-sexo enum('M', 'F'),
-peso decimal(5,2), 
-altura decimal(3,2), 
-nacionalidade varchar(20) default 'Brasil'
-primary key (id) 
+	id int not null auto_increment, 
+	nome varchar(30) not null, 
+	nascimento date,
+	sexo enum('M', 'F'),
+	peso decimal(5,2), 
+	altura decimal(3,2), 
+	nacionalidade varchar(20) default 'Brasil'
+	primary key (id) 
 ) default charset = utf8; 
 
